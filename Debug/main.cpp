11759a7396d53a5ec107fbe285920ff62e954b86
magurmach <008.shakib@gmail.com>
2015-03-23 00:56:50 +0600

    Initial Commit

---
+++ Debug/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Debug
+//
+//  Created by Shakib Ahmed on 3/23/15.
+//  Copyright (c) 2015 Shakib Ahmed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
